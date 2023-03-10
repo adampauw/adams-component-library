25155563c4c52db7bb3f6ee1552a2452c183b84b
adam pauw <adampauw@gmail.com>
2023-03-09 18:32:36 -0800

    small doc manager style tweak

--- src/Screens/DocumentManager/DocumentManager.tsx
+++ src/Screens/DocumentManager/DocumentManager.tsx
@@ -2,36 +2,7 @@ import { Collapse } from '@mui/material';
 import { useEffect, useRef, useState } from 'react';
 
 import { DataType, Document, DocumentCard, DocumentEditor, DOCUMENTS } from '../../__stdlib';
-import { VF_BLACK } from '../../constants/Colours.constants';
-import { makeStyles } from '../../Helpers/makeStyles';
-
-const useStyles = ({ isOpen }: { isOpen: boolean }) =>
-  makeStyles({
-    container: {
-      display: 'flex',
-      backgroundColor: '#bfabf0',
-      justifyContent: 'space-between',
-    },
-    itemContainer: {
-      display: 'grid',
-      gridTemplateColumns: isOpen ? 'repeat(3, minmax(250px, 1fr))' : 'repeat(4, minmax(calc(100vw/4), 1fr))',
-    },
-    item: {
-      margin: 20,
-      width: 250,
-    },
-    activeItem: {
-      outline: `2px solid ${VF_BLACK}`,
-    },
-    sidebar: {
-      width: 300,
-      height: '100%',
-      backgroundColor: '#f6c3cc',
-    },
-    collapseList: {
-      minWidth: 'unset!important',
-    },
-  });
+import { useStyles } from './DocumentManager.styles';
 
 function DocumentManager() {
   const [isOpen, setIsOpen] = useState(false);
